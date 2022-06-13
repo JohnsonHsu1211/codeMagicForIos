178020871ec23721d0a357c5edf1b768f38115bd
JohnsonHsu1211 <johnson.hsu1211@gmail.com>
2022-06-13 13:54:52 +0800

    first commit

---
+++ CodeMagicDemo/CodeMagicDemoApp.swift
@@ -0,0 +1,17 @@
+//
+//  CodeMagicDemoApp.swift
+//  CodeMagicDemo
+//
+//  Created by Johnson Hsu on 2022/6/13.
+//
+
+import SwiftUI
+
+@main
+struct CodeMagicDemoApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
---
+++ CodeMagicDemo/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  CodeMagicDemo
+//
+//  Created by Johnson Hsu on 2022/6/13.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, Johnon!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
